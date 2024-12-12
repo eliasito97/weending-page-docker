00cc823b1f08ab961e03403e45868ca7ee813362
Elias <eliaschavarriab@usip.edu.bo>
2024-12-12 13:53:41 -0400

    ultimo v2

--- src/components/Attendance/Attendance.tsx
+++ src/components/Attendance/Attendance.tsx
@@ -13,8 +13,8 @@ function Attendance() {
 				Tu presencia es importante para nosotros, por favor confirma tu
 				asistencia hasta el 20 de Diciembre
 			</p>
-			<div className="flex flex-row mb-10">
-				<CiCalendarDate className="inline text-5xl title"/>
+			<div className="flex md:flex-row flex-col mb-10 items-center">
+				<CiCalendarDate className="inline text-5xl title "/>
 				<MapButton
 					link="https://api.whatsapp.com/send?phone=59177480070&text=%C2%A1Hola%20Boris!%20%F0%9F%91%8B%20%C2%A1Confirmo%20mi%20asistencia%20a%20la%20boda!%20%F0%9F%8E%89%20Estoy%20emocionado%2Fa%20de%20celebrar%20este%20d%C3%ADa%20tan%20especial%20junto%20a%20ustedes.%20%C2%A1Nos%20vemos%20pronto!%20%F0%9F%91%B0%F0%9F%A4%B5%20%20Nombre%3A"
 					label="Confirma con el Novio"
@@ -33,17 +33,20 @@ function Attendance() {
 			<p className="text-4xl title mb-10">
 				Será un honor contar con tu presencia.
 			</p>
-			<h3 className="font-semibold text-5xl title mb-10">
+			<h3 className="font-semibold text-5xl title">
 				¡Te esperamos!
 			</h3>
-			<Image
-				src={"/assets/2.png"}
-				alt={"Frame 2"}
-				width={1200}
-				height={1200}
-				className="w-auto h-auto rounded-2xl ayuda1"
-				priority
-			/>
+			<div className="relative md:ml-60 md:-top-10 mt-5 md:mt-0">
+				<Image
+					src={"/assets/2.png"}
+					alt={"Frame 2"}
+					width={1200}
+					height={1200}
+					className="w-auto h-auto rounded-2xl"
+					priority
+				/>
+			</div>
+
 		</div>
 	);
 }
--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -4,15 +4,18 @@ import { FaHeart } from "react-icons/fa";
 export function Header() {
 	return (
 		<div className="container flex flex-col items-center mx-auto px-5 mb-10">
-			<Image
-				src={"/assets/1.png"}
-				alt={"Frame 1"}
-				width={1200}
-				height={1200}
-				className="w-auto h-auto mb-10 rounded-2xl ayuda"
-				priority
-			/>
-			<div className="max-w-3xl ayuda2 text-center items-center">
+			<div className="relative md:mr-60 md:top-10">
+				<Image
+					src={"/assets/1.png"}
+					alt={"Frame 1"}
+					width={500}
+					height={500}
+					className="rounded-2xl pe-8 justify-start"
+					priority
+				/>
+			</div>
+
+			<div className="max-w-3xl text-center items-center">
 				<h1 className="mb-10 text-5xl title">
 					Nuestra Boda
 				</h1>
@@ -20,24 +23,6 @@ export function Header() {
 					Cindy & Boris
 				</h2>
 			</div>
-			{/*<div className="text-center items-center m-5 text-4xl">*/}
-			{/*	<p className="mb-10 title">*/}
-			{/*		Presiona el*/}
-			{/*		<span className="pr-3 pl-4">*/}
-			{/*			<FaHeart className="inline text-blue-900 text-1xl" />*/}
-			{/*		</span>*/}
-			{/*		para escuchar nuestra canción*/}
-			{/*	</p>*/}
-			{/*	<MusicPlayer />*/}
-			{/*</div>*/}
-			{/*<Image*/}
-			{/*	src={"/assets/banner.jpg"}*/}
-			{/*	alt={"Frame 1"}*/}
-			{/*	width={1200}*/}
-			{/*	height={1200}*/}
-			{/*	className="w-auto h-auto rounded-2xl mb-8"*/}
-			{/*	priority*/}
-			{/*/>*/}
 			<p className="text-center text-4xl title">
 				TENEMOS EL HONOR DE INVITARLOS
 				A NUESTRA CEREMONIA RELIGIOSA Y CIVIL EL PROXIMO
